e10503a8afae04fca4d4f3e3a2a818dedb671c93
Vladislav762 <70996164+Vladislav762@users.noreply.github.com>
2021-05-30 12:30:15 +0600

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,27 +1,22 @@
 age={1:'3-7 лет',2:'5-10 лет',3:'8-14 лет'}
 class room:
     total_cost=0
-class toy1:
+class toy1():
     def __init__(self):
     self.cost=randint(400,2000)
     self.group=group'3-7 лет'
     total_cost=self.cost
-    count+=1
-class toy1:
-    def __init__(self):
-    self.cost=randint(400,2000)
-    self.group=group'3-7 лет'
-    total_cost=self.cost
-    count+=1
-class toy2:
+    count= count+1
+class toy2():
     def __init__(self):
     self.cost=randint(400,2000)
     self.group=group'5-10 лет'
     total_cost=self.cost
-    count+=1
-class toy3:
+    count=count+1
+class toy3():
     def __init__(self):
     self.cost=randint(400,2000)
     self.group=group'8-14 лет'
     total_cost=self.cost
-    count+=1
+    count=count+1
+    
