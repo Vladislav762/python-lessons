a746f39b8a8a65fff382e690d58173dff85e4f60
vladislav <mirage9231@gmail.com>
2021-05-30 12:21:27 +0600

    работа в классе

---
+++ 1.py
@@ -0,0 +1,27 @@
+age={1:'3-7 лет',2:'5-10 лет',3:'8-14 лет'}
+class room:
+    total_cost=0
+class toy1:
+    def __init__(self):
+    self.cost=randint(400,2000)
+    self.group=group'3-7 лет'
+    total_cost=self.cost
+    count+=1
+class toy1:
+    def __init__(self):
+    self.cost=randint(400,2000)
+    self.group=group'3-7 лет'
+    total_cost=self.cost
+    count+=1
+class toy2:
+    def __init__(self):
+    self.cost=randint(400,2000)
+    self.group=group'5-10 лет'
+    total_cost=self.cost
+    count+=1
+class toy3:
+    def __init__(self):
+    self.cost=randint(400,2000)
+    self.group=group'8-14 лет'
+    total_cost=self.cost
+    count+=1
